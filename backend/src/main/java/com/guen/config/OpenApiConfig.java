e4960db55eb147c87de6baa52f22e009801cfe42
geun <kkaddus@naver.com>
2024-01-22 09:52:39 +0900

    feat:openApi 설정 변경

--- backend/src/main/java/com/guen/config/OpenApiConfig.java
+++ backend/src/main/java/com/guen/config/OpenApiConfig.java
@@ -1,23 +1,23 @@
 package com.guen.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition
 @Configuration
-public class SpringdocConfig {
+public class OpenApiConfig {
 
     @Bean
     public OpenAPI baseOpenAPI() {
-        return new OpenAPI().info(
-                new Info()
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
                         .title("Backend Project")
                         .version("1.0.0.")
                         .description("기본에 충실하자. 가능한 많은 것들을 다뤄 보자.")
-        );
+                );
     }
 }
 
