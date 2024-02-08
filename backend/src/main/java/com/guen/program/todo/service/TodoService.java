8154cf3ca0f9b5e81fc64cc57d531bd85f033f0f
geun <kkaddus@naver.com>
2024-02-08 11:33:49 +0900

    feat:소스 정리

--- backend/src/main/java/com/guen/program/todo/service/TodoService.java
+++ backend/src/main/java/com/guen/program/todo/service/TodoService.java
@@ -35,6 +35,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class TodoService {
 
     private final FileStorageService fileStorageService;
